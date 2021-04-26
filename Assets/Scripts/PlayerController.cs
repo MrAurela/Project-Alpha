72f7e1176ad795b5f97e92306d7ab9520e181cfe
Leo P <leopeltola@gmail.com>
2021-04-26 12:31:21 +0300

    collisions work but fps super low

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,29 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movSpeed = 1;
+    public float moveSpeed = 1;
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
 
     // Update is called once per frame
     void Update()
     {
-        var inputVector = new Vector3(0, 0, 0);
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-        inputVector.Normalize();
-        transform.position += inputVector * Time.deltaTime * movSpeed;
+        processInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void processInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
     }
 }
