78d4f546ffa7b8d314d5516e83c3977f4378ab53
aurelam1 <miroaurela@gmail.com>
2021-05-14 20:38:27 +0300

    Changes to sprite selection.

--- Assets/Scripts/SpriteSelector.cs
+++ Assets/Scripts/SpriteSelector.cs
@@ -13,29 +13,39 @@ public class SpriteSelector : MonoBehaviour
     public bool down = false;
     public bool left = false;
 
+    public string upS;
+    public string rightS;
+    public string downS;
+    public string leftS;
+
     void Update()
     {
         int spriteIndex = 0;
-        if (!HasObjectInDirection("Floor", Vector2.up))
+        if (!HasObjectInDirection("Floor", "Default", Vector2.up) || HasObjectInDirection("Wall", "Wall", Vector2.up))
         {
             spriteIndex += 1;
             up = true;
         }
-        if (!HasObjectInDirection("Floor", Vector2.right))
+        if (!HasObjectInDirection("Floor", "Default", Vector2.right) || HasObjectInDirection("Wall", "Wall", Vector2.right))
         {
             spriteIndex += 2;
             right = true;
         }
-        if (!HasObjectInDirection("Floor", Vector2.down))
+        if (!HasObjectInDirection("Floor", "Default", Vector2.down) || HasObjectInDirection("Wall", "Wall", Vector2.down))
         {
             spriteIndex += 4;
             down = true;
         }
-        if (!HasObjectInDirection("Floor", Vector2.left))
+        if (!HasObjectInDirection("Floor", "Default", Vector2.left) || HasObjectInDirection("Wall", "Wall", Vector2.left))
         {
             spriteIndex += 8;
             left = true;
         }
+
+        upS = ObjectInDirection(Vector2.up);
+        rightS = ObjectInDirection(Vector2.right);
+        downS = ObjectInDirection(Vector2.down);
+        leftS = ObjectInDirection(Vector2.left);
 
         GetComponent<SpriteRenderer>().sprite = sprites[spriteIndex];
 
@@ -44,16 +54,28 @@ public class SpriteSelector : MonoBehaviour
             GetComponent<SpriteRenderer>().sprite = downRightSprite;
         }*/
 
-        if (HasObjectInDirection("Door", Vector2.down)) {
+        if (HasObjectInDirection("Door", "Door", Vector2.down)) {
             GetComponent<SpriteRenderer>().sprite = overDoorSprite;
         }
     }
 
 
-    private bool HasObjectInDirection(string tag, Vector2 direction)
+    private bool HasObjectInDirection(string tag, string layer, Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.gameObject.transform.position, direction, 1f, LayerMask.GetMask(layer));
+        return hit.collider != null && hit.collider.gameObject.tag == tag;
+    }
+
+    private string ObjectInDirection(Vector2 direction)
     {
         RaycastHit2D hit = Physics2D.Raycast(this.gameObject.transform.position, direction, 1f);
-        return hit.collider != null && hit.collider.gameObject.tag == tag;
+        if (hit.collider)
+        {
+            return hit.collider.gameObject.tag;
+        } else
+        {
+            return "";
+        }
     }
 
     public bool HasObjectInDirection(int directionId)
