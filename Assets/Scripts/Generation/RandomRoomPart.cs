c5a4b70ffa2fa1bf77ea2b3dfe8f1ed2d1dc3424
aurelam1 <miroaurela@gmail.com>
2021-05-18 22:25:10 +0300

    Changed back to normal version.

--- Assets/Scripts/Generation/RandomRoomPart.cs
+++ Assets/Scripts/Generation/RandomRoomPart.cs
@@ -15,7 +15,7 @@ public class RandomRoomPart : MonoBehaviour
         RoomGeneration roomGenerator = transform.root.GetComponent<RoomGeneration>();
         Room room = roomGenerator.room;
         QRand qrand = FindObjectOfType<QRand>();
-        qrand.InitState(room.Seed + (int)transform.position.x * 10000000 + (int)transform.position.y * 1000000000); //Change the seed based on the location
+        qrand.InitState(room.Seed); //Change the seed based on the location
         float random = qrand.NextFloat();
 
         //Random.InitState(room.Seed); //TODO something addition to make new combinations
@@ -30,6 +30,7 @@ public class RandomRoomPart : MonoBehaviour
             {
                 GameObject roomPart = Instantiate(roomParts[i], transform.position, Quaternion.identity, transform.parent);
                 //roomPart.transform.parent = roomGenerator.transform.Find("Layout").transform;
+                roomPart.transform.parent = gameObject.transform;
                 break;
             }
         }
@@ -45,16 +46,24 @@ public class RandomRoomPart : MonoBehaviour
             {
                 if (i == 0 || i == 2) x = true;
                 if (i == 1 || i == 2) y = true;
+                Debug.Log("Probs: "+i);
             }
         }
 
-        for (int i = 0; i < gameObject.transform.childCount; i++)
-        {
-            GameObject child = gameObject.transform.GetChild(i).gameObject;
-            if (x) child.transform.localPosition = new Vector3(-child.transform.localPosition.x, child.transform.localPosition.y, child.transform.localPosition.z);
-            if (y) child.transform.localPosition = new Vector3(child.transform.localPosition.x, -child.transform.localPosition.y, child.transform.localPosition.z);
-            child.transform.parent = roomGenerator.transform.Find("Layout").transform;
+        Debug.Log(x + ", " + y);
+
+        int j = 0;
+        if (transform.childCount > 0) {
+            foreach (Transform child in transform.GetChild(0))
+            {
+                if (x) child.localPosition = new Vector3(-child.localPosition.x, child.localPosition.y, child.localPosition.z);
+                if (y) child.localPosition = new Vector3(child.localPosition.x, -child.localPosition.y, child.localPosition.z);
+                child.parent = roomGenerator.transform.Find("Layout").transform;
+                Debug.Log("Childs: " + j);
+                j += 1;
+            }
         }
+        
 
         //This game object is not neede anymore
         Destroy(gameObject);
